dbe10dbd40956c5f0de5a67e9f2f5f2ccd00e58f
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-18 03:20:17 +0530

    Amstrong Number

---
+++ Basic Question PRactics/AmstrongNumber.cpp
@@ -0,0 +1,28 @@
+// -- -- -- -- -- -- -- -- -- -- -- -- -Armstrong number-- -- -- -- -- -- -- -- -- -- -- -- -- -an n - digit number is an Armstrong number if the sum of its digits, each raised to the nth power, is equal to the number itself.153 = 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 153.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 153;
+    int reverseNumber = 0;
+    int ORiginNumber = number;
+    int sum = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        //    number=number/10;
+        sum = sum + (lastDigit * lastDigit * lastDigit);
+        number = number / 10;
+    }
+    if (sum == ORiginNumber)
+    {
+        cout << "Armstrong number ";
+    }
+    else
+        cout << "Not a Armstrong number ";
+
+    return 0;
+}
+
+// Armstrong number
