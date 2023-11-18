38bbe9b2549ca74c0410ab40a397f3a2fc534c07
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-19 03:04:50 +0530

    check Polindrom of String

---
+++ Basic Question PRactics/Recursion in C++/Array DATA STRUCTURE/String Problem/PolindromOFstring.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#include <string> 
+bool Polindrom(string s){
+    int length=s.length();
+    for(int i=0;i<s.length/2;i++){
+        return true;
+    }
+
+}
+
+
+
+int main(){
+string mystring="MADAM";
+
+
+
+    return 0;
+}
