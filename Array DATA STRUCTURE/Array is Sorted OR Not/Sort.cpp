f1d21c20324556663535dff8a8385ab4e3cfbde9
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-23 04:28:50 +0530

    check array is sorted or not

---
+++ Array DATA STRUCTURE/Array is Sorted OR Not/Sort.cpp
@@ -0,0 +1,50 @@
+// Question:Checking array is sorted
+//  int arr1[] = {1, 2, 3, 4, 5};
+//     int arr2[] = {5, 4, 3, 2, 1};
+
+// output:
+//  In summary:Array1 is Sorted";
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+using namespace std;
+
+int IsSorted(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i - 1] > arr[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int arr2[] = {5, 4, 3, 2, 1};
+
+    if (IsSorted(arr, 5))
+    {
+        cout << "Array one is Sorted";
+    }
+    else
+    {
+        cout << "Array is not sorted";
+    }
+
+    if (IsSorted(arr2, 5))
+    {
+        cout << "Array2 one is Sorted";
+    }
+    else
+    {
+        cout << "Array2 is not sorted";
+    }
+
+    return 0;
+}
