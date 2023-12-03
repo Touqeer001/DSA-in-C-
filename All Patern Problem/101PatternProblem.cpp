c0078c1a95543d3a8efeaf1569e35ce083663146
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-03 15:03:19 +0530

    update

--- All Patern Problem/101PatternProblem.cpp
+++ All Patern Problem/101PatternProblem.cpp
@@ -28,13 +28,13 @@ int main()
 {
 
     int start = 1;
-    for (int i = 2; i <= 5; i++)
+    for (int i = 0; i <= 5; i++)
     {
         if (i % 2 == 0)
             start = 1;
         else
             start = 0;
-        for (int j = 2; j <= i; j++)
+        for (int j = 0; j <= i; j++)
         {
             cout << start;
             start = 1 - start;
