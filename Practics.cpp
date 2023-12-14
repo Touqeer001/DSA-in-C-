afbc7a96a8a3fe0aa29e5fca20c9cf797c4f92cf
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-14 20:01:44 +0530

    Practics array question

--- Practics.cpp
+++ Practics.cpp
@@ -7,8 +7,12 @@ using namespace std;
 int main()
 
 {
-    string name = "Touqeer";
-    cout << name;
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " "; // Print each element of the array
+    }
     return 0;
 }
