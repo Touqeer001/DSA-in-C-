cff4f3b0aed04a5c09666a901c8a9ae8d790de45
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-22 07:56:31 +0530

    anagram

--- String in C++/c++ anagram problem/angram.cpp
+++ String in C++/c++ anagram problem/angram.cpp
@@ -3,6 +3,17 @@
 // std::string s = "anagram";
 //     std::string t = "nagaram";
 
+// Two strings are considered anagrams if they have the same characters with the same frequency, even if the characters are arranged in a different order.
+// For example, the strings "listen" and "silent" are anagrams because they use the same set of characters with the same frequency, just in a different order.
+
+// Here's the general approach to solving this problem:
+
+// Check Lengths: If the lengths of the two strings are different, they cannot be anagrams. In this case, return false.
+
+// Character Frequency Counting: Use a data structure (like an unordered map) to count the frequency of each character in the first string (s). Then, iterate through the second string (t) and decrement the count for each character encountered.
+
+// Check Counters: After processing both strings, check if all character counts are zero. If they are, then the strings are anagrams; otherwise, they are not.
+
 #include <iostream>
 using namespace std;
 #include <unordered_map>
