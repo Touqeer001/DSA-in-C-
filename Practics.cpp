c400ddecf1140569763e8da8fa06d64ae6b5a394
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-25 18:45:58 +0530

    practics

--- Practics.cpp
+++ Practics.cpp
@@ -10,14 +10,7 @@ using namespace std;
 int main()
 
 {
-    for (int i = 1; i <= 5; i++)
-    {
-        for (int j = 1; j < 5; j++)
-        {
-            cout << i;
-        }
-        cout << endl;
-    }
+    cout << "Touqeer ansari";
 
     return 0;
 }
