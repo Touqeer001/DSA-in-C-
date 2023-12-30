efed7159bc198ddb60f4a6ab406a616c2be1ace3
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-30 22:12:25 +0530

    function practics

--- Practics.cpp
+++ Practics.cpp
@@ -3,10 +3,16 @@
 #include <iostream>
 using namespace std;
 #include <string>
+int add(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 
 {
+    int result = add(10, 10);
+    cout << result;
 
     return 0;
 }
