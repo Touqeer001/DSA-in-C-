7fbdde83e2babfc35b185460dcf0f52badd14963
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-03 21:43:24 +0530

    practics

--- Practics.cpp
+++ Practics.cpp
@@ -2,11 +2,33 @@
 
 #include <iostream>
 using namespace std;
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
 #include <string>
 
 int main()
 
 {
+    int start = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        if (i % 2 == 0)
+        {
+            start = 1;
+        }
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
