83ec4215570722c1d78cbb04a67c0911d3df9b8b
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-09 21:30:06 +0530

    practics.cpp

--- Practics.cpp
+++ Practics.cpp
@@ -1,17 +1,3 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-#include <vector>
-<<<<<<< HEAD
-
-int main()
-{
-    cout << "Touqeer ansari ";
-
-    return 0;
-}
-=======
-int main(){
-  cout<<"Hello Word...."
-  return 0;
-}
->>>>>>> 446a99d9f35593bf37c39f238927b1980e0ba846
+int main
