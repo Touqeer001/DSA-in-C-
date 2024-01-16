b66a64500f769c989930e5f22b63e79fdc0e2bbe
Touqeer Ansari <touqeeransari001@gmail.com>
2024-01-16 22:15:47 +0530

    practics

--- Practics.cpp
+++ Practics.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
-  cout << "Find the sorted array...";
-=======
 
->>>>>>> e176a4f0f45b7ed52e229648978f6c854a556254
+  cout << "Find the sorted array...";
+
   return 0;
 }
