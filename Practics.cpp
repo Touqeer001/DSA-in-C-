a6fb0847299de167e8c8f76f2dbf2c7e9e1605a6
Touqeer Ansari <touqeeransari001@gmail.com>
2024-05-01 21:53:52 +0530

    prractics

--- Practics.cpp
+++ Practics.cpp
@@ -3,15 +3,14 @@ using namespace std;
 int main()
 {
     int a;
-    cout << "Enter any Number : 1
-    ";
+    cout << "Enter any Number : ";
     cin >> a;
 
     int b;
     cout << "Enter Second numbeer : ";
     cin >> b;
     int sum = a + b;
-    cout << "Sum of Two numer are : " << sum;
+    cout << "Sum of numer are : " << sum;
  
     return 0;
 }
